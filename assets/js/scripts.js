b9c7c711bb76bee29cb998fcbdf518c6794c6fe0
Juan C. Coello <juanc.coellop@gmail.com>
2014-09-04 23:20:24 +0200

    def coming soon page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,19 +2,10 @@
 jQuery(document).ready(function() {
 
     /*
-        Background slideshow
-    */
-//    $('.coming-soon').backstretch([
-//      "assets/img/backgrounds/1.jpg"
-//    , "assets/img/backgrounds/2.jpg"
-//    , "assets/img/backgrounds/3.jpg"
-//    ], {duration: 3000, fade: 750});
-
-    /*
         Countdown initializer
     */
     var now = new Date();
-    var countTo = 25 * 24 * 60 * 60 * 1000 + now.valueOf();
+    var countTo = new Date(2014,10,04).valueOf();
     $('.timer').countdown(countTo, function(event) {
         var $this = $(this);
         switch(event.type) {
@@ -31,48 +22,5 @@ jQuery(document).ready(function() {
                 break;
         }
     });
-
-    /*
-        Tooltips
-    */
-    $('.social a.facebook').tooltip();
-    $('.social a.twitter').tooltip();
-    $('.social a.dribbble').tooltip();
-    $('.social a.googleplus').tooltip();
-    $('.social a.pinterest').tooltip();
-    $('.social a.flickr').tooltip();
-
-    /*
-        Subscription form
-    */
-    $('.success-message').hide();
-    $('.error-message').hide();
-
-    $('.subscribe form').submit(function() {
-        var postdata = $('.subscribe form').serialize();
-        $.ajax({
-            type: 'POST',
-            url: 'assets/sendmail.php',
-            data: postdata,
-            dataType: 'json',
-            success: function(json) {
-                if(json.valid == 0) {
-                    $('.success-message').hide();
-                    $('.error-message').hide();
-                    $('.error-message').html(json.message);
-                    $('.error-message').fadeIn();
-                }
-                else {
-                    $('.error-message').hide();
-                    $('.success-message').hide();
-                    $('.subscribe form').hide();
-                    $('.success-message').html(json.message);
-                    $('.success-message').fadeIn();
-                }
-            }
-        });
-        return false;
-    });
-
 });
 

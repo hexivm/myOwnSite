25277b09e4786979994ad08b92158b141ae9f8a2
juanccoellop <juanc.coellop@gmail.com>
2016-03-03 23:23:28 +0100

    new page

---
+++ js/init.js
@@ -0,0 +1,46 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+    $('.do-not-push').click(function(){
+        console.log(`
+                                            ,:
+                                          ,' |
+                                         /   :
+                                      --'   /
+                                      \\/ />/
+                                      / /_\\
+                                   __/   /
+                                   )'-. /
+                                   ./  :\\
+                                    /.' '
+                                  '/'
+                                  +
+                                 '
+                               \`.
+                           .-"-
+                          (    |
+                       . .-'  '.
+                      ( (.   )8:
+                  .'    / (_  )
+                   _. :(.   )8P  \`
+               .  (  \`-' (  \`.   .
+                .  :  (   .a8a)
+               /_\`( "a \`a. )"'
+           (  (/  .  ' )=='
+          (   (    )  .8"   +
+            (\`'8a.( _(   (
+         ..-. \`8P    ) \`  )  +
+       -'   (      -ab:  )
+     '    _  \`    (8P"Ya
+   _(    (    )b  -\`.  ) +
+  ( 8)  ( _.aP" _a   \( \   *
++  )/    (8P   (88    )  )
+   (a:f   "     \`"       \`
+
+        `);
+    });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
